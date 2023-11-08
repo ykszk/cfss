fc2aa1b6333ff8236ca224c4e1ec8adac81d42f4
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2023-11-08 11:11:59 +0900

    Add distance live updating

--- cfss/shape_stats.py
+++ cfss/shape_stats.py
@@ -1,31 +1,25 @@
 import argparse
+import os
 import sys
+from functools import wraps
 from pathlib import Path
+from typing import Callable
 
 import numpy as np
 import PyQt5
 import vtk
-
-# import vtkmodules.vtkRenderingOpenGL2
-from landmark import load_landmarks, locate_landmarks
 from logzero import logger
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget
 from ssm import PCAStats
-from utils import read_mesh, write_mesh
 
-# from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+# vtk
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from vtkmodules.util.numpy_support import numpy_to_vtk, vtk_to_numpy
 from vtkmodules.vtkCommonColor import vtkNamedColors
-from vtkmodules.vtkCommonCore import vtkCommand, vtkIdTypeArray, vtkPoints
-from vtkmodules.vtkCommonDataModel import (
-    vtkPolyData,
-    vtkSelection,
-    vtkSelectionNode,
-    vtkUnstructuredGrid,
-)
+from vtkmodules.vtkCommonCore import vtkIdTypeArray
+from vtkmodules.vtkCommonDataModel import vtkSelection, vtkSelectionNode
 from vtkmodules.vtkFiltersCore import vtkGlyph3D, vtkPolyDataNormals
 from vtkmodules.vtkFiltersExtraction import vtkExtractSelection
 from vtkmodules.vtkFiltersSources import vtkSphereSource
@@ -36,12 +30,25 @@ from vtkmodules.vtkRenderingCore import (
     vtkHardwarePicker,
     vtkPolyDataMapper,
     vtkRenderer,
-    vtkRenderWindow,
-    vtkRenderWindowInteractor,
 )
 
 
+logger.setLevel(os.environ.get('LOGLEVEL', 'INFO'))
+
 # https://github.com/pyvista/pyvista/discussions/4781
+
+
+def left_button_on_exit(method):
+    '''
+    Call `self.OnLeftButtonDown()` on exit
+    '''
+
+    @wraps(method)
+    def _impl(self, *method_args, **method_kwargs):
+        method(self, *method_args, **method_kwargs)
+        self.OnLeftButtonDown()
+
+    return _impl
 
 
 # Catch mouse events
@@ -51,50 +58,52 @@ class MouseInteractorStyle(vtkInteractorStyleTrackballCamera):
         self.data = data
         self.selected_mapper = vtkDataSetMapper()
         self.selected_actor = vtkActor()
+        self.selected_actor.GetProperty().SetColor(vtkNamedColors().GetColor3d('Red'))
+        self.selected_actor.SetMapper(self.selected_mapper)
+        picker = vtkHardwarePicker()
+        picker.SnapToMeshPointOn()
+        picker.SetPixelTolerance(5)
+        self.picker = picker
         sphere_source = vtkSphereSource()
         self.source = sphere_source
         self.source.SetPhiResolution(11)
         self.source.SetThetaResolution(11)
         self.source.SetRadius(2)
+
         self.pids = [0, 0]
         ids = vtkIdTypeArray()
         ids.SetNumberOfComponents(1)
         ids.SetNumberOfValues(2)
         self.ids = ids
-        self.index = None
-
-        self.scalar = numpy_to_vtk(np.array([[0], [1]]).astype(np.float32))
+        self.pick_index = None
+        self.on_change = []
 
     def set_index(self, index: int):
-        self.index = index
+        self.pick_index = index
 
+    def add_on_change(self, callback: Callable):
+        self.on_change.append(callback)
+
+    @left_button_on_exit
     def left_button_press_event(self, obj, event):
-        if self.index is None:
-            self.OnLeftButtonDown()
+        if self.pick_index is None:
             return
-        colors = vtkNamedColors()
+
+        picker = self.picker
 
         # Get the location of the click (in window coordinates)
         pos = self.GetInteractor().GetEventPosition()
-
-        # picker = vtkCellPicker()
-        picker = vtkHardwarePicker()
-        picker.SnapToMeshPointOn()
-        picker.SetPixelTolerance(5)
-        # picker.UseCellsOn()
-        # picker.SetTolerance(0.01)
 
         # Pick from this location.
         picker.Pick(pos[0], pos[1], 0, self.GetDefaultRenderer())
         pid = picker.GetPointId()
         if pid != -1:
-            logger.info(f'Point for {self.index}: {pid}')
-            # print(f'Pick position is: ({world_position[0]:.6g}, {world_position[1]:.6g}, {world_position[2]:.6g})')
+            logger.info(f'Picked point for {self.pick_index}: {pid}')
 
-            self.pids[self.index] = pid
+            self.pids[self.pick_index] = pid
             self.ids.SetValue(0, self.pids[0])
             self.ids.SetValue(1, self.pids[1])
-            self.index = None
+            self.pick_index = None
 
             selection_node = vtkSelectionNode()
             selection_node.SetFieldType(vtkSelectionNode.POINT)
@@ -115,14 +124,20 @@ class MouseInteractorStyle(vtkInteractorStyleTrackballCamera):
             # glyph3D.SetColorModeToColorByScalar()
 
             self.selected_mapper.SetInputConnection(glyph3D.GetOutputPort())
-            self.selected_mapper.ScalarVisibilityOn()
-            self.selected_actor.SetMapper(self.selected_mapper)
-            self.selected_actor.GetProperty().SetColor(colors.GetColor3d('Red'))
+            # self.selected_mapper.ScalarVisibilityOn()
 
             self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer().AddActor(self.selected_actor)
+            for cb in self.on_change:
+                cb()
 
-        # Forward events
-        self.OnLeftButtonDown()
+
+def make_parser():
+    parser = argparse.ArgumentParser(description='Shape stats.')
+    parser.add_argument('-i', '--input', help='Input directory. default: %(default)s', default='../result/lm_aligned')
+    parser.add_argument(
+        '--morph_max', help='Morph slider\'s maximum value. default: %(default)s', default=100, type=int
+    )
+    return parser
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -130,10 +145,12 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.setWindowTitle('CFDB Browser')
 
-        logger.info('Create pca')
-        indir = Path('../result/lm_aligned')
+        args = make_parser().parse_args()
+
+        logger.info('Create PCA')
+        indir = Path(args.input)
         filenames = sorted(indir.glob('*.vtk'))
-        pca_stats = PCAStats.from_files(filenames)
+        pca_stats, mesh = PCAStats.from_files(filenames)
         logger.info('Done')
 
         self.frame = QtWidgets.QWidget()
@@ -144,7 +161,6 @@ class MainWindow(QtWidgets.QMainWindow):
 
         colors = vtkNamedColors()
 
-        mesh = read_mesh('../result/ssm/ssm.vtk')
         mesh_points = vtk_to_numpy(mesh.GetPoints().GetData())
 
         mapper = vtkPolyDataMapper()
@@ -221,15 +237,11 @@ class MainWindow(QtWidgets.QMainWindow):
         button.clicked.connect(pick_callback(1))
         group.layout().addWidget(button)
 
-        button = QtWidgets.QPushButton('Calculate', self)
-        group.layout().addWidget(button)
-
         calc_result = QtWidgets.QLabel('', self)
 
         def calc_callback():
-            logger.info(f'Calc stats for {style.pids[0]} and {style.pids[1]}')
+            logger.debug(f'Calc stats for {style.pids[0]} and {style.pids[1]}')
             dists = pca_stats.dists_between(style.pids[0], style.pids[1])
-            # print(dists)
             p1 = np.array(mesh.GetPoint(style.pids[0]))
             p2 = np.array(mesh.GetPoint(style.pids[1]))
             cur_dist = np.linalg.norm(p1 - p2)
@@ -239,31 +251,30 @@ class MainWindow(QtWidgets.QMainWindow):
             text = '\n'.join([text1, text2])
             calc_result.setText(text)
 
-        button.clicked.connect(calc_callback)
+        style.add_on_change(calc_callback)
         group.layout().addWidget(calc_result)
 
-        group = QtWidgets.QGroupBox('Warp', self)
+        group = QtWidgets.QGroupBox('Morph', self)
         dock_widget.layout().addWidget(group)
         group.setLayout(QtWidgets.QVBoxLayout())
 
         data_list = ['average'] + [f'case {i+1}' for i in range(len(pca_stats.components))]
         slider = QtWidgets.QSlider(Qt.Orientation.Horizontal)
-        slider.setMaximum(100)
-        slider.setMinimum(0)
+        slider.setMaximum(max(100, args.morph_max))
+        slider.setMinimum(min(0, 0 - (args.morph_max - 100)))
         slider.setValue(0)
-        warp_boxes = []
+        morph_boxes = []
         for i in range(2):
             combobox = QtWidgets.QComboBox(self)
             combobox.addItems(data_list)
 
             def callback(_):
                 # change value from 1 to 0 to make sure to CHANGE the value to 0
-                slider.setValue(1)
+                slider.setValue(1)  # TODO: fix this workaround
                 slider.setValue(0)
-                # slider.triggerAction(slider.valueChanged)
 
             combobox.currentIndexChanged.connect(callback)
-            warp_boxes.append(combobox)
+            morph_boxes.append(combobox)
             group.layout().addWidget(QtWidgets.QLabel(f'Data{i+1}', self))
             group.layout().addWidget(combobox)
 
@@ -272,9 +283,10 @@ class MainWindow(QtWidgets.QMainWindow):
         normal_filter.SplittingOff()
 
         def slider_callback():
+            slider.setToolTip(f'{slider.value()}')
             value = slider.value() / 100
-            idx1, idx2 = warp_boxes[0].currentIndex(), warp_boxes[1].currentIndex()
-            # logger.info(f'warp: {value}, {idx1}, {idx2}')
+            idx1, idx2 = morph_boxes[0].currentIndex(), morph_boxes[1].currentIndex()
+            # mean shape is `idx1 == 0 `
             zeros = np.zeros_like(pca_stats.all_coefs[0])
             coef1 = zeros if idx1 == 0 else pca_stats.all_coefs[idx1 - 1]
             coef2 = zeros if idx2 == 0 else pca_stats.all_coefs[idx2 - 1]
@@ -288,6 +300,7 @@ class MainWindow(QtWidgets.QMainWindow):
             mesh.GetCellData().SetNormals(normals.GetCellData().GetNormals())  # TODO: is this actually working?
             mesh.Modified()
             ren_win.Render()
+            calc_callback()
 
         slider.valueChanged.connect(slider_callback)
         group.layout().addWidget(slider)
